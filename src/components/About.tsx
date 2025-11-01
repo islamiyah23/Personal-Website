eb1848fb1889da0ad1b453751ec4db18807471f1
islamiyah23 <islamiyahdinark@gmail.com>
2025-11-01 17:38:16 +0800

    About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,15 +3,13 @@ export default function About() {
 		<section id="about" className="space-y-6">
 			<h3 className="text-sm uppercase tracking-wider font-medium text-zinc-500 dark:text-zinc-300">About</h3>
 			<p className="text-zinc-800 dark:text-zinc-200">
-				Ruben Amorim adalah pelatih sepak bola asal Portugal yang kini menukangi Manchester United.
-				Dikenal dengan filosofi permainan menyerang dan pressing intensitas tinggi, Amorim membawa pendekatan taktik modern
-				yang berakar dari pengalamannya bersama Sporting CP. Ia menekankan kerja sama tim, disiplin posisi,
-				serta pengembangan pemain muda dalam setiap proyeknya.
+				Saya adalah mahasiswa Program Studi Teknik Informatika angkatan 2023 di Sekolah Tinggi Teknologi Bontang.
+				Saya memiliki ketertarikan besar pada dunia teknologi dan pengembangan sistem informasi.
+
 			</p>
 			<p className="text-zinc-800 dark:text-zinc-200">
-				Saat ini, Amorim fokus membangun kembali identitas permainan Manchester United dengan gaya dinamis,
-				cepat dalam transisi, dan berorientasi pada penguasaan bola. Tujuannya jelas mengembalikan kejayaan klub
-				di panggung domestik maupun Eropa.
+				Di luar bidang akademik, saya gemar bermain bulu tangkis sebagai cara untuk menjaga kebugaran dan melatih konsentrasi.
+				Selain itu, saya juga senang memasak di rumah dan sering mencoba berbagai resep baru sebagai bentuk kreativitas serta eksplorasi rasa.
 			</p>
 		</section>
 	);
